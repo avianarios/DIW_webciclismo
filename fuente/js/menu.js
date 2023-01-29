49c838832b5fba8e1c86b5b6d126ecd27e1e51e7
avianarios <43814416+avianarios@users.noreply.github.com>
2023-01-29 23:11:01 +0100

    commit inicial

---
+++ fuente/js/menu.js
@@ -0,0 +1,19 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+/*Se controla el despliegue del menú añadiéndole una clase "active". Cuando existe, el menú se despliega. Cuando no, se repliega
+Esta función hace que si la lista ul que forma el menú no tiene la clase "active" la añade y si la tiene, la quita. También cambia el icono según esté deplegada o no*/
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
